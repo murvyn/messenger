15fa978c1be6880750b01f969beeb9b400a47aaf
Murvyn <marvin.asamoah.123@gmail.com>
2024-02-26 15:52:42 +0000

    initail commit

---
+++ messager/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React, { useState } from 'react'
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState()
+  return (
+    <div></div>
+  )
+}
+
+export default AuthForm
