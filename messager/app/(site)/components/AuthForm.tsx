1b01ab51c0a710291c653e79318ad52b50752abe
Murvyn <marvin.asamoah.123@gmail.com>
2024-02-29 04:39:28 +0000

    prisma schema

--- messager/app/(site)/components/AuthForm.tsx
+++ messager/app/(site)/components/AuthForm.tsx
@@ -1,7 +1,10 @@
 "use client";
+import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -9,7 +12,7 @@ const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
-  const toggleVarient = useCallback(() => {
+  const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
       setVariant("REGISTER");
     } else {
@@ -55,24 +58,82 @@ const AuthForm = () => {
               label="Name"
               register={register}
               type="text"
+              disable={isLoading}
             />
-            )}
-            <Input
-              errors={errors}
-              id="email"
-              label="Email address"
-              register={register}
-              type="email"
-            />
-            <Input
-              errors={errors}
-              id="password"
-              label="Password"
-              register={register}
-              type="password"
-            />
-            
+          )}
+          <Input
+            errors={errors}
+            id="email"
+            label="Email address"
+            register={register}
+            type="email"
+            disable={isLoading}
+          />
+          <Input
+            errors={errors}
+            id="password"
+            label="Password"
+            register={register}
+            type="password"
+            disable={isLoading}
+          />
+          <Button disable={isLoading} fullWidth type="submit">
+            {variant === "LOGIN" ? "Sign in" : "Register"}
+          </Button>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+            icon={BsGithub}
+            onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+            icon={BsGoogle}
+            onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div
+          className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div
+            onClick={toggleVariant}
+            className="underline cursor-pointer"
+          >
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
